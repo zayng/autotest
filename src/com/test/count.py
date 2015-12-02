6762d71607b027f755c3bad9c9987f4640e23d03
sha-m12152249 <orcexzy@163.com>
2015-12-02 19:51:26 +0800

    add unittest

---
+++ src/com/test/count.py
@@ -0,0 +1,15 @@
+#-*- coding:utf-8 -*-
+'''
+Created on 2015年12月2日
+
+@author: 119937
+'''
+class count():
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+        
+    def add(self):
+        return self.a+self.b
+    
+    
