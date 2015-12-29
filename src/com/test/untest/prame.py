50aecc383e03da2588d8e90f981d55abffcadaeb
sha-m12152249 <orcexzy@163.com>
2015-12-29 12:41:13 +0800

    update

--- src/com/test/untest/prame.py
+++ src/com/test/untest/prame.py
@@ -3,9 +3,10 @@ Created on '2015/12/29'
 
 @author: '119937'
 """
-dat = []
+
 
 def is_prime(n):
+    dat = []
     if n <= 1:
         return False
     else:
--- src/com/test/untest/utest.py
+++ src/com/test/untest/utest.py
@@ -12,16 +12,14 @@ class MyTestCase(unittest.TestCase):
         print("Startting test.")
 
     def test_prame7(self):
-
         self.assertTrue(is_prime(7), msg="It's not Prime numbers")
+        print()
         print("Prime numbers")
 
     def test_prame8(self):
-
         self.assertTrue(is_prime(8), msg="It's not Prime numbers")
         print("Prime numbers1")
     def test_prame9(self):
-
         self.assertTrue(is_prime(9), msg="It's not Prime numbers")
         print("Prime numbers2")
 
