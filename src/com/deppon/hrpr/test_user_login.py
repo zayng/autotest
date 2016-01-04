bc4a217964ea32b4f095c7682f062c47403c0012
sha-m12152249 <orcexzy@163.com>
2016-01-04 19:11:36 +0800

    add page object mode

---
+++ src/com/deppon/hrpr/test_user_login.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+"""
+Created on '2016/1/4'
+
+@author: '119937'
+"""
+from time import sleep
+from com.deppon.hrpr.loginpage import Login126
+
+def test_user_login(driver, username, password):
+    """测试登陆126邮箱
+    :param driver:
+    :param username:
+    :param password:
+    :return:
+    """
+    login_page = Login126(driver)
+    login_page.open()
+    login_page.type_user(username)
+    login_page.type_pwd(password)
+    login_page.submit()
+    sleep(3)
+    assert(username == 'suesce@163.com'), "用户名称不匹配，登陆失败"
