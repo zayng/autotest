4c7f16f9a32ec9ff5c3550a0bca3cb9056c6eb6b
sufy <orcexzy@163.com>
2016-01-05 12:05:06 +0800

    update loginpage

--- src/com/deppon/hrpr/LoginPage.py
+++ src/com/deppon/hrpr/LoginPage.py
@@ -27,14 +27,14 @@ class Login126(Page):
     def open(self):
         self._open(self.uri)
 
-    # def type_user(self, user):
-    #     self.find_element(*self.username_loc).send_keys(user)
-    #
-    # def type_pwd(self, pwd):
-    #     self.find_element(*self.password_loc).send_keys(pwd)
-    #
-    # def submit(self):
-    #     self.find_element(*self.loginbtn_loc).click()
+    def type_user(self, user):
+        self.find_element(*self.username_loc).send_keys(user)
+
+    def type_pwd(self, pwd):
+        self.find_element(*self.password_loc).send_keys(pwd)
+
+    def submit(self):
+        self.find_element(*self.loginbtn_loc).click()
 
 
 if __name__ == 'main__':
